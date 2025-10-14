a677708942bf4c9caeb2cc03c848a93ac98dd926
HalenaAquino <hjaym5@umkc.edu>
2025-10-14 15:57:21 -0500

    Added blue and red player S/O options. Board throws an error and refuses to generate if the board size and game mode is invalid.

--- CS449_SOS_Project/src/sprint_2/GUI.java
+++ CS449_SOS_Project/src/sprint_2/GUI.java
@@ -8,12 +8,16 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import java.util.Scanner;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Font;
+
+import javafx.scene.paint.Color;
+
+
 
 public class GUI extends Application {
 	
@@ -33,21 +37,30 @@ public class GUI extends Application {
 		GridPane bottomPane = new GridPane();		// record checkbox, player turn
 		
 		
+		
+		
+		GridPane centerLPane = new GridPane();
+		GridPane centerCPane = new GridPane();
+		//centerCPane.setPrefHeight(500);
+		centerCPane.setPrefWidth(500);
+		GridPane centerRPane = new GridPane();
+		
+		
 		// Creates the game choice radio buttons and adds them to the top plane
 		RadioButton simpleRButton = new RadioButton("Simple Game");
 		RadioButton generalRButton = new RadioButton("General Game");
-		ToggleGroup group = new ToggleGroup();
-		simpleRButton.setToggleGroup(group);
-		generalRButton.setToggleGroup(group);
+		ToggleGroup gameButtonGroup = new ToggleGroup();
+		simpleRButton.setToggleGroup(gameButtonGroup);
+		generalRButton.setToggleGroup(gameButtonGroup);
 		generalRButton.setTranslateX(30);
 		topPane.add(simpleRButton, 1, 5);
 		topPane.add(generalRButton, 2, 5);
 		
 		// Creates a textbox for the board size and adds it to the top plane
-		Label label1 = new Label("Board Size:");
+		Label boardSize = new Label("Board Size:");
 		TextField textField = new TextField ();
 		HBox hb = new HBox();
-		hb.getChildren().addAll(label1, textField);
+		hb.getChildren().addAll(boardSize, textField);
 		hb.setSpacing(5);
 		hb.setTranslateX(120);
 		topPane.add(hb,  3,  5);
@@ -58,27 +71,93 @@ public class GUI extends Application {
 		topPane.add(button, 4, 5);
 		button.setTranslateX(120);
 		
+		Label errorMessage = new Label("");
+
+		
 		// Changes the size of the board based on the user entered number
 		button.setOnAction(event -> {			// TODO: add conditional to do nothing if the textbox is empty
 			int size =  Integer.parseInt(textField.getText());
 
-			if (size > 0 && size < 10) {
+			if (size > 0 && size < 10 && (simpleRButton.isSelected() || generalRButton.isSelected())) {
+				centerCPane.getChildren().clear();
 				squares = new Square[size][size];
 				for (int i = 0; i < size; i++)
 					for (int j = 0; j < size; j++)
-						centerPane.add(squares[i][j] = new Square(), j, i);
-		    }});
+						centerCPane.add(squares[i][j] = new Square(size), j, i);
+				
+				errorMessage.setText("");
+		    }
+			else{
+				errorMessage.setText("Please enter a valid board size and select a game mode");
+			}});
+			
 		
-		CheckBox myCheckBox = new CheckBox();
-		myCheckBox.setText("Record game");
-		Label gameStatus = new Label("Player One's Turn");   // TODO: make dynamic
-		bottomPane.add(myCheckBox, 1, 5);
-		bottomPane.add(gameStatus, 2, 5);
-		gameStatus.setTranslateX(120);
+		
+		Label gameStatus = new Label("Blue Players Turn");   // TODO: make dynamic
+		bottomPane.add(gameStatus, 1, 5);
+		bottomPane.add(errorMessage, 2, 5);
+		gameStatus.setTranslateX(300);
+		errorMessage.setTranslateX(100);
+		errorMessage.setTranslateY(-25);
+		
+		errorMessage.setTextFill(Color.RED);
+		errorMessage.setFont(new Font("Arial", 15));
+				
+		
+		
+		
+		Label bluePlayerLabel = new Label("Blue Player: ");
+		bluePlayerLabel.setTranslateX(-25);
+		bluePlayerLabel.setMinWidth(65);
+		bluePlayerLabel.setTranslateY(200);
+		RadioButton blueSButton = new RadioButton("S");
+		RadioButton blueOButton = new RadioButton("O");
+		ToggleGroup bluePieceGroup = new ToggleGroup();
+		blueSButton.setToggleGroup(bluePieceGroup);
+		blueOButton.setToggleGroup(bluePieceGroup);
+		blueSButton.setTranslateX(bluePlayerLabel.getTranslateX() - 40);
+		blueSButton.setTranslateY(bluePlayerLabel.getTranslateY() + 30);
+		blueOButton.setTranslateX(blueSButton.getTranslateX() - 28);
+		blueOButton.setTranslateY(blueSButton.getTranslateY() + 25);
+		
+		centerCPane.setTranslateX(bluePlayerLabel.getMaxWidth() - 60);
+		//centerCPane.setTranslateY(50000000);
 		
 
+		
+		Label redPlayerLabel = new Label("Red Player: ");
+		redPlayerLabel.setTranslateX(centerCPane.getMaxWidth() - 40);
+		redPlayerLabel.setMinWidth(65);
+		redPlayerLabel.setTranslateY(200);
+		RadioButton redSButton = new RadioButton("S");
+		RadioButton redOButton = new RadioButton("O");
+		ToggleGroup redPieceGroup = new ToggleGroup();
+		redSButton.setToggleGroup(redPieceGroup);
+		redOButton.setToggleGroup(redPieceGroup);
+		redSButton.setTranslateX(redPlayerLabel.getTranslateX() - 40);
+		redSButton.setTranslateY(redPlayerLabel.getTranslateY() + 30);
+		redOButton.setTranslateX(redSButton.getTranslateX() - 28);
+		redOButton.setTranslateY(redSButton.getTranslateY() + 25);
+		
+		
+		centerLPane.add(bluePlayerLabel, 1, 5);
+		centerLPane.add(blueSButton, 2, 5);
+		centerLPane.add(blueOButton, 3, 5);
+		centerRPane.add(redPlayerLabel, 1, 5);
+		centerRPane.add(redSButton, 2, 5);
+		centerRPane.add(redOButton, 3, 5);
+		
+		centerPane.add(centerLPane, 1, 5);
+		centerPane.add(centerCPane, 2, 5);
+		centerPane.add(centerRPane, 3, 5);
+		
+
+		
+		
+		
+		
 		// Adds each pane to the border pane
-		centerPane.setTranslateY(30);
+		centerCPane.setTranslateY(30);
 		centerPane.setTranslateX(50);			// TODO: make board location dynamic (depending on its size)
 		BorderPane borderPane = new BorderPane();
 		borderPane.setCenter(centerPane);
@@ -86,16 +165,17 @@ public class GUI extends Application {
 		borderPane.setTop(topPane);
 				
 		// Creates the scene with the border plane
-		Scene scene = new Scene(borderPane, 600, 600);
+		Scene scene = new Scene(borderPane, 700, 700);
 		primaryStage.setTitle("SOS Game");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
+
 	public class Square extends Pane {
-		public Square() {
+		public Square(int size) {
 			setStyle("-fx-border-color: black");
-			this.setPrefSize(100, 100);			// TODO: make window size dynamic
+			this.setPrefSize(500/size, 500/size);			// TODO: make window size dynamic
 			this.setOnMouseClicked(e -> handleMouseClick());
 		}
 
