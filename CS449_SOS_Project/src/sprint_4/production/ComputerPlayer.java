33d87a8283e03f454e23ca7ca3a45dcac45abaa5
HalenaAquino <hjaym5@umkc.edu>
2025-11-10 10:52:29 -0600

    scrapped "Player" class idea, basing code on TTT autoplay example

--- CS449_SOS_Project/src/sprint_4/production/ComputerPlayer.java
+++
@@ -1,9 +0,0 @@
-package sprint_4.production;
-
-
-public class ComputerPlayer extends Player{
-	
-	public  ComputerPlayer(char c) {
-		super(c);
-	}
-}
--- CS449_SOS_Project/src/sprint_4/production/HumanPlayer.java
+++
@@ -1,9 +0,0 @@
-package sprint_4.production;
-
-
-public class HumanPlayer extends Player {
-	
-	public  HumanPlayer(char c) {
-		super(c);
-	}
-}
