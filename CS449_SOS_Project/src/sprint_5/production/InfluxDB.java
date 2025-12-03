87df536f2b4fb6512d3299547f56496bb88a6f6e
HalenaAquino <hjaym5@umkc.edu>
2025-12-03 15:28:32 -0600

    working on influx write, about to implement it into GUI

--- CS449_SOS_Project/src/sprint_5/production/InfluxDB.java
+++ CS449_SOS_Project/src/sprint_5/production/InfluxDB.java
@@ -17,64 +17,51 @@ public class InfluxDB {
     private static final String token = "X3nk1vTvEhNh3B13fNWXVa0H9LKKU3W6ROX9xTfurTcBCZpE6iq4Jwvi7MxVJdHA0X7yn_xpDhCXhcsIWJqEAA==";
     private static final String org = "SOSGame";
     private static final String bucket = "SOSGameBucket";
-    private InfluxDBClient client = InfluxDBClientFactory.create(url, token.toCharArray(), org, bucket);
+    private static InfluxDBClient client = null;
     
-    public InfluxDBClient connect() {
-        return InfluxDBClientFactory.create(url, token.toCharArray(), org, bucket);
+    public static InfluxDBClient connect() {
+    	if (client == null) {
+            client = InfluxDBClientFactory.create(url, token.toCharArray(), org, bucket);
+        }
+        return client;
     }
 	
-	public void write(Player p, Cell move, char piece) {
-		try {
-            // Create a data point
-            Point point = Point.measurement("temperature")
-                    .addTag("location", "roomTest2")
-                    .addField("test2", 0.50)
-                    .time(Instant.now(), WritePrecision.NS);
-            
-            // Write the point
-            WriteApiBlocking writeApi = client.getWriteApiBlocking();
-            writeApi.writePoint(point);
-            
-            System.out.println("✅ Data written successfully!");
-            
-        } catch (Exception e) {
-            System.err.println("❌ Error writing data: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            client.close();
-        }
+	public static void write(String player, String piece, int xCoord, int yCoord) {
+		// create the row
+		Point point = Point.measurement("SOSGameRecord")
+				.addField("Player", player)
+				.addField("Piece", piece)
+				.addField("x_coordinate", xCoord)
+				.addField("y_coordinate", yCoord)
+				.time(Instant.now(), WritePrecision.NS);
+		
+		// Write the point
+		WriteApiBlocking writeApi = connect().getWriteApiBlocking();
+		writeApi.writePoint(point);            
 	}
 	
 	public void query() {
-		// Connect to InfluxDB
-        
-        try {
-            // Flux query to retrieve data from the last hour
-            String flux = "from(bucket: \"SOSGameBucket\")" +
-                         " |> range(start: -1h)" +
-                         " |> filter(fn: (r) => r._measurement == \"temperature\")";
-            
-            // Execute query
-            List<FluxTable> tables = client.getQueryApi().query(flux);
-            
-            // Display results
-            System.out.println("📊 Query Results:");
-            for (FluxTable table : tables) {
-                for (FluxRecord record : table.getRecords()) {
-                    System.out.println(
-                        "Time: " + record.getTime() + 
-                        " | Location: " + record.getValueByKey("location") +
-                        " | Value: " + record.getValue()
-                    );
-                }
-            }
-            
-        } catch (Exception e) {
-            System.err.println("❌ Error querying data: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            client.close();
-        }
+		//TODO
+	}
+	
+	public void clearTable() {
+		//TODO
+	}
+	
+	
+	
+	
+	// used to test write
+	public static void main(String[] args) {
+		write("Blue", "S", 0, 0);
+		write("Red", "O", 2, 4);
+		write("Blue", "O", 1, 1);
+		write("Red", "S", 2, 1);
+		
+		client.close();
+		client = null;
+		
+		System.out.println("rows added");
 	}
 		
 }
